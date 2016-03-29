131f3de9086500315ec53557312ca9dad520d297
Delta256 <connor@madgarden.net>
2016-03-29 09:54:31 -0400

    Test commit MK II

--- app/src/main/java/com/madgarden/connor/mediatest/Mediaplayer.java
+++ app/src/main/java/com/madgarden/connor/mediatest/Mediaplayer.java
@@ -3,16 +3,9 @@ package com.madgarden.connor.mediatest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.io.IOException;
-
-import android.app.Activity;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.Toast;
+
 
 public class Mediaplayer extends AppCompatActivity  {
 
@@ -29,8 +22,7 @@ public class Mediaplayer extends AppCompatActivity  {
     public static void main(String [] args)throws IOException {
 
         //hacking together a music playing function below
-
-        String url = "HTTP GOES HERE";
+        String url = "http://soundbible.com/grab.php?id=1986&type=mp3";
         MediaPlayer mediaPlayer = new MediaPlayer();
 
         mediaPlayer.reset();
