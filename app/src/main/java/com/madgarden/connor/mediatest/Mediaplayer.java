04d3bd4278bfd83d486e172f90548897cb047987
Delta256 <connor@madgarden.net>
2016-03-31 13:23:18 -0400

    Test MK VI
    Apparently the whole project hasn't been pushed to git, this will hopefully fix that issue.

--- app/src/main/java/com/madgarden/connor/mediatest/Mediaplayer.java
+++ app/src/main/java/com/madgarden/connor/mediatest/Mediaplayer.java
@@ -28,14 +28,14 @@ public class Mediaplayer extends AppCompatActivity  {
 
         //hacking together a music playing function below
 
-        String url = "http://soundbible.com/grab.php?id=1986&type=mp3";
+        String sUrl = "http://soundbible.com/grab.php?id=1986&type=mp3"; //URL for a generic explosion sound
         MediaPlayer mPlayer = new MediaPlayer();
 
         mPlayer.reset();
         System.out.println("Resetting");
         mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         System.out.println("Setting stream type");
-        mPlayer.setDataSource(url);
+        mPlayer.setDataSource(sUrl);
         System.out.println("Grabbing from URL");
         mPlayer.prepare();
         System.out.println("Preparing");
